da75b97665781b25af85d15fdb817fb378d4d88b
Lars <lg@lars.systems>
2016-02-26 23:16:20 +0100

    init

---
+++ NosDataExtractor/NosDataExtractor/NosExtractor.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NosDataExtractor
+{
+    /// <summary>
+    /// Many thanks to the awesome guys on epvp.
+    /// I do not take ownership of the following code. 
+    /// I for my self didnt revealed nor released the decryption methods.
+    /// </summary>
+    class NosExtractor
+    {
+        private readonly Action<string> _log;
+
+        public NosExtractor(Action<string> log)
+        {
+            _log = log;
+        }
+
+        public bool Extract(string sourceFilename, DirectoryInfo storedirectory)
+        {
+            try
+            {
+                BinaryReader br = new BinaryReader(File.OpenRead(sourceFilename));
+
+                int numFiles = br.ReadInt32();
+                _log("## numFiles=" + numFiles);
+
+                for (int fnum = 0; fnum < numFiles; fnum++)
+                {
+                    int fileCount = br.ReadInt32();
+                    int fileNameSize = br.ReadInt32();
+                    string fileName = Encoding.ASCII.GetString(br.ReadBytes(fileNameSize));
+                    int fileCryptoMode = br.ReadInt32();
+                    int fileSize = br.ReadInt32();
+                    byte[] fileData = br.ReadBytes(fileSize);
+                    if (fileCryptoMode == 1)
+                    {
+                        byte[] newdata = decryptnosfile(fileData);
+                        //_log(string.Format("decrypted={1},{0}", Encoding.UTF7.GetString(newdata).Substring(0, 30), newdata.Length));
+                        fileData = newdata;
+                    }
+                    else
+                    {
+                        //_log("does not need to be decrypted.");
+                    }
+
+                    _log("{" + $"{fileCount},{fileNameSize},{fileName},{fileCryptoMode},{fileSize}" + "}");
+
+                    storedirectory.Create();
+                    using (BinaryWriter bw = new BinaryWriter(File.OpenWrite(Path.Combine(storedirectory.FullName, fileName))))
+                    {
+                        bw.Write(fileData);
+                        bw.Flush();
+                        bw.Close();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                _log("NOS File invalid.");
+                return false;
+            }
+            return true;
+        }
+
+        private byte[] decryptnosfile(byte[] readInData)
+        {
+            var table = new byte[] { 0, 0, 45, 46, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 13 };
+            int readlength = readInData.Length;
+            List<byte> cOut = new List<byte>();
+
+            int offset = 0;
+            while (offset < readlength)
+            {
+                if (readInData[offset] == 255)
+                {
+                    offset++;
+                    cOut.Add(13);
+                    continue;
+                }
+
+                byte size = readInData[offset++];
+
+                var i = 0;
+                if ((size & 128) == 0)
+                {
+                    while ((i++ < size) && (offset < readlength))
+                        cOut.Add((byte)(readInData[offset++] ^ 51));
+                }
+                else
+                {
+                    size &= 127;
+                    while ((i < size) && (offset < readlength))
+                    {
+                        byte c = readInData[offset++];
+
+                        cOut.Add(table[(c & 240) >> 4]);
+
+                        if (table[c & 15] != 0)
+                            cOut.Add(table[c & 15]);
+
+                        i += 2;
+                    }
+                }
+            }
+            return cOut.ToArray();
+        }
+    }
+}
---
+++ NosDataExtractor/NosDataExtractor/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NosDataExtractor
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.Title = "Nostale Data Extractor";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Valid .NOS-Files are:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("  NScliData");
+            Console.WriteLine("  NSetcData");
+            Console.WriteLine("  NSgtdData - Main stuff like item.dat");
+            Console.WriteLine("  NSlangData - Your Language stuff");
+            Console.WriteLine("And maybe some more.");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine();
+            Thread.Sleep(2000);
+
+            while (true)
+            {
+                Console.WriteLine("Source?");
+
+                OpenFileDialog ofd = new OpenFileDialog
+                {
+                    Filter = "NOS-File|*.NOS",
+                    CheckFileExists = true
+                };
+                DialogResult dialogResult = ofd.ShowDialog();
+
+                if (dialogResult != DialogResult.OK || string.IsNullOrEmpty(ofd.FileName))
+                    break;
+
+
+                Console.WriteLine("Target?");
+
+                FolderBrowserDialog fbd = new FolderBrowserDialog
+                {
+                    ShowNewFolderButton = true,
+                    SelectedPath = new FileInfo(ofd.FileName).DirectoryName
+                };
+                dialogResult = fbd.ShowDialog();
+
+                if (dialogResult != DialogResult.OK || string.IsNullOrEmpty(fbd.SelectedPath))
+                    break;
+
+
+                Console.WriteLine("Starting...");
+
+                try
+                {
+                    NosExtractor ne = new NosExtractor(Console.WriteLine);
+                    ne.Extract(ofd.FileName, new DirectoryInfo(Path.Combine(fbd.SelectedPath, new FileInfo(ofd.FileName).Name.Replace(".NOS", ""))));
+
+                    Console.WriteLine("Finished.");
+                    Console.WriteLine();
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("NOS File invalid.");
+                    Console.ResetColor();
+                }
+
+                Console.WriteLine("Restart...");
+                Thread.Sleep(1000);
+                Console.WriteLine("\n\n");
+            }
+
+            Console.WriteLine("end of program.");
+            Thread.Sleep(2000);
+        }
+    }
+}
